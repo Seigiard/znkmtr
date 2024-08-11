57c6c41950d2909e4e4cff4b6e4823318ef56994
Andrew Borisenko <seigiard@gmail.com>
2024-08-11 11:42:32 +0200

    Add new tile

--- js/map.js
+++ js/map.js
@@ -1,6 +1,13 @@
+const attributeLabels = [
+    '<a href="https://leafletjs.com" title="A JavaScript library for interactive maps"><svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="12" height="8" viewBox="0 0 12 8" class="leaflet-attribution-flag"><path fill="#4C7BE1" d="M0 0h12v4H0z"></path><path fill="#FFD500" d="M0 4h12v3H0z"></path><path fill="#E0BC00" d="M0 7h12v1H0z"></path></svg> Leaflet</a>',
+    '<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    '<a href="https://carto.com/attributions">CARTO</a>'
+].join(' / ');
+
 initMap({
     center: [46.4849, 30.7419],
-    zoom: 20
+    zoom: 19, // 20 max
+    attributionControl: false
 });
 
 function initMap(mapOptions) {
@@ -10,10 +17,16 @@ function initMap(mapOptions) {
     var map = new L.map(mapElementId, mapOptions);
 
     // Creating a Layer object
-    var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+    // var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+    // var layer = new L.TileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png');
+    // var layer = new L.TileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png');
+    var layer = new L.TileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png');
+
 
     // Adding layer to the map
     map.addLayer(layer);
+
+    L.control.attribution({ prefix: attributeLabels }).addTo(map)
 
     L.control.ruler({
         position: 'topleft',
